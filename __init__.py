f93dbe911294fba6b65aed97e1da4a1a62ea03cb
Dmitiris <dmitry@unipat.ru>
2018-01-11 17:34:14 +0300

    Adding read config object

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from fnc_walker import main as walker
 from fnc_mkdir import main as mkdir
 from cls_dbcon import DBCon as DBC
 from cls_log import LogObj as LOG
+from cls_readconf import RCONF as CONF
---
+++ cls_readconf.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from re import match
+
+class RCONF():
+    def __init__(self, filepath):
+        self.filepath = filepath
+        attrib = self.readconf()
+        self.keys = attrib.keys()
+        for a in attrib:
+            setattr(self, a, attrib[a])
+
+
+    def readconf(self):
+        res = {}
+        with open(self.filepath) as f:
+            data = f.read()
+            lines = data.split('\n')  # should make support for \r
+            for line in lines:
+                g = match('(\w+)\s*=\s*(\w+)', line)
+                if g:
+                    res[str(g.group(1))] = str(g.group(2))
+        return res
+
+
+    def __str__(self):
+        return 'Configuration object from "%s"' % self.filepath
+
+
+    def __repr__(self):
+        return self.__str__()
