9bc750c4358e3a7f84234784b268abcc9982ff4e
dmitry <dmitry@unipat.ru>
2018-01-12 16:48:03 +0300

    Adding emailer to package; renaming classes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from fnc_walker import main as walker
 from fnc_mkdir import main as mkdir
-from cls_dbcon import DBCon as DBC
-from cls_log import LogObj as LOG
-from cls_readconf import RCONF as CONF
+from cls_dbcon import DBCon as DBConObject
+from cls_log import LogObj as LogObject
+from cls_readconf import RCONF as ReadConfig
+from cls_emailer import Sender as Emailer
