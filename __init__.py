d15417a75bc17964af2b6646079a317b89ecd2c8
dmitry <dmitry@unipat.ru>
2018-02-02 05:19:58 +0300

    Added fnc_readargs

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from fnc_walker import main as walker
 from fnc_mkdir import main as mkdir
+from fnc_readargs import main as readargs
 from cls_dbcon import DBCon as DBConObject
 from cls_log import LogObj as LogObject
 from cls_readconf import RCONF as ReadConfig
