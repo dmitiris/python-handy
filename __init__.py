af107f8a58070be7765911ea0b6a2cdaf0e24492
Dmitry <dmitry@unipat.ru>
2018-05-08 18:06:43 +0300

    Changed cls_dbcon, DBCon object gets depreciated, should use DBConnection class

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from fnc_walker import main as walker
 from fnc_mkdir import main as mkdir
 from fnc_readargs import main as readargs
 from cls_dbcon import DBCon as DBConObject
+from cls_dbcon import DBConnection as DBConClass
 from cls_log import LogObj as LogObject
 from cls_log import LOG as LogClass
 from cls_readconf import RCONF as ReadConfig
--- cls_dbcon.py
+++ cls_dbcon.py
@@ -105,4 +105,64 @@ class DB:
         return self.CUR.statusmessage
 
 
+class DBConnection:
+    def __init__(self, db_name='postgres', db_user='postgres', db_word=False, db_host='localhost', db_port=5432):
+        keywords = ['dbname', 'user', 'password', 'host', 'port']
+        data = [db_name, db_user, db_word, db_host, db_port]
+        connection_string_parts = ['%s=%s' % (keywords[i], data[i]) for i in range(0, len(data)) if data[i]]
+        self.connection_string = ' '.join(connection_string_parts)
+
+    def execute(self, sql_request, sql_data=(), output=True):
+        with connect(self.connection_string) as con:
+            cur = con.cursor()
+            if sql_data:
+                cur.execute(sql_request, sql_data)
+            else:
+                cur.execute(sql_request)
+            if output:
+                data = cur.fetchall()
+            else:
+                data = str(cur.statusmessage)
+            con.commit()
+        return data
+
+    def bulk_upsert(self, table, columns, unique, data, schema='public'):
+        """ data = [{}, {}, {}] """
+        # print 'DATA:', data
+        if len(data) == 0:
+            return None      # Just in case data is empty
+        pcolumns = ', '.join(columns)
+        value_plc = ','.join(['(%s)' % ','.join(['%s' for ghost in item]) for item in data])
+        punique = ', '.join(unique)
+        update = ', '.join(['%s=EXCLUDED.%s' % (column, column) for column in columns])
+        sql_query = """
+            INSERT INTO %(schema)s.%(table)s (%(columns)s) VALUES %(value_placeholder)s
+            ON CONFLICT (%(unique)s) DO UPDATE SET %(update)s;
+            """ % {
+            'schema': schema, 'table': table, 'columns': pcolumns,
+            'value_placeholder': value_plc, 'unique': punique, 'update': update
+        }
+        """"""
+        sql_data = []
+        if isinstance(data[0], dict):
+            sql_data = [sublist[item] for sublist in data for item in columns]
+        elif isinstance(data[0], list):
+            sql_data = [item for sublist in data for item in sublist]
+        result = self.execute(sql_query, sql_data, output=False)
+        return result
+
+    def bulk_delete(self, table, data, data_key, column_key=False, schema='public'):
+        if len(data) == 0:
+            return 0
+        if not column_key:
+            column_key = data_key
+        keys = ', '.join(["'%s'::text" % item[data_key] for item in data])
+        sql_query = """ 
+            DELETE FROM %(schema)s.%(table)s WHERE ARRAY[%(unique)s::text] <@ ARRAY[%(keys)s] 
+            """ % {'schema': schema, 'table': table, 'unique': column_key, 'keys': keys}
+        """"""
+        result = self.execute(sql_query, output=False)
+        return result
+
+
 DBCon = DB()
