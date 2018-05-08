de95ee30280825ff4e196e8401fe2e07b2dbbecb
Dmitry <dmitry@unipat.ru>
2018-05-08 17:15:47 +0300

    Making LogClass available for import

--- __init__.py
+++ __init__.py
@@ -4,5 +4,6 @@ from fnc_mkdir import main as mkdir
 from fnc_readargs import main as readargs
 from cls_dbcon import DBCon as DBConObject
 from cls_log import LogObj as LogObject
+from cls_log import LOG as LogClass
 from cls_readconf import RCONF as ReadConfig
 from cls_emailer import Sender as Emailer
